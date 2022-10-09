4eb1f6a1aa9675adb98bcb3b67f05b04bbf1c98f
Aiden Faulconer <aidenf09@yahoo.com>
2022-10-09 12:11:02 +1100

    +restructure +husky +eslint +hasura-auth (WIP)

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders table of actors', () => {
   render(<App />);
